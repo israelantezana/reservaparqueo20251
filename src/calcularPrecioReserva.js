f46a09f842bc638656ce72dd6d785343049fffb4
Israel Antezana <israelantezana@gmail.com>
2025-03-18 10:05:24 -0400

    Feat: lanza error si la hora es menor a 6

--- src/calcularPrecioReserva.js
+++ src/calcularPrecioReserva.js
@@ -1,6 +1,9 @@
 function calcularPrecioReserva(hora, tiempo) {
-    return 5;
-    
+    const precioHora=5;
+    if (hora <6 ) {
+        throw new Error("Error, hora no permitida")
+    }
+    return tiempo*precioHora;
   }
   
 export default calcularPrecioReserva;
--- src/calcularPrecioReserva.spec.js
+++ src/calcularPrecioReserva.spec.js
@@ -7,6 +7,17 @@ describe("CalcularPrecioReserva", () => {
   it("deberia calcular la reserva para una hora en horario permitido", () => {
     expect(calcularPrecioReserva(6, 1)).toEqual(5);
   });
+
+  it("deberia calcular la reserva para dos horas en horario permitido", () => {
+    expect(calcularPrecioReserva(7, 2)).toEqual(10);
+  });
+
+  //(2) Caso reservas fuera de hora:
+  //  calcularPrecioReseva(5, 1) =>  "Error, hora no permitida"
+  //  calcularPrecioReseva(11, 2) =>  "Error, hora no permitida"
+  it("deberia mostrar error si la hora es menor a las 6am", () => {
+    expect(()=> calcularPrecioReserva(5, 1)).toThrow("Error, hora no permitida");
+  });
 });
 
 
